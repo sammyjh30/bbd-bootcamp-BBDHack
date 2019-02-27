1b26d9bd146e98e984da5fe4d10abf2998332ab2
Supreme Senpai Zungula <supremez@bbd.co.za>
2019-02-27 18:15:27 +0200

    Can connect to the DB and add or retrieves a user.

--- facial-recognition-demo/src/app/app.js
+++ facial-recognition-demo/src/app/app.js
@@ -2,7 +2,11 @@ const express = require('express');
 const app = express();
 const mssql = require('mssql')
 
-const db_config = {
+// app.listen(4200, () => {
+//     console.log("Server started on port: 4200");
+// });
+
+const config = {
     server : 'localhost',
     password : 'whatever',
     user : 'YamkelaZ',
@@ -10,12 +14,43 @@ const db_config = {
     port: 50092
 };
 
-const pool = new mssql.ConnectionPool(db_config);
- pool.connect((err, result) => {
-     if (err){
-         console.log('Error could not connect to the database');
-     }
-    else {
-        console.log("CONNECTED");
+(async function () {
+    try {
+        let pool = await mssql.connect(config)
+        console.dir("Connected to the database");
+    } catch (err) {
+        // ... error checks
+        console.error(err);;
     }
- })
+
+    async function get_user(user_id){
+        let result1 = await pool.request()
+        .input('uid', mssql.Int, user_id)
+        .query('SELECT * from Users WHERE userID = @uid')
+        return (result1);
+    }
+
+
+    function add_user(uid, firstname, lastname, position)
+    {
+        const request = new mssql.Request()
+        request.query("INSERT INTO Users (firstName, lastName, position) VALUES "+"('"+uid+"',"+firstname+"', '"+lastname+"', '"+position+"')")
+        .then(result => {
+            console.log(result.rowsAffected)
+        })
+    }
+
+    function add_user_images(user_id, f_image, r_image, l_image){
+        const request = new mssql.Request()
+        request.query("INSERT INTO Reference_images (userID, front_side, right_side, left_side) VALUES ('"+user_id+"', '"+f_image+"', '"+r_image+"','"+l_image+"'')")
+        .then(result => {
+            console.log(result.rowsAffected)
+        })
+    }
+    
+})()
+
+mssql.on('error', err => {
+    // ... error handle
+    console.log("An error occured");
+})
