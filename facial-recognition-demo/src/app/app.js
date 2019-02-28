ada618101eb2fb0d2f4ab144b347f41c630e6c18
Supreme Senpai Zungula <supremez@bbd.co.za>
2019-02-28 10:43:55 +0200

    needs to wait for results before continuing.

--- facial-recognition-demo/src/app/app.js
+++ facial-recognition-demo/src/app/app.js
@@ -20,37 +20,52 @@ const config = {
         console.dir("Connected to the database");
     } catch (err) {
         // ... error checks
+        console.log("ERROR: could not connect to the database");
         console.error(err);;
     }
 
-    async function get_user(user_id){
-        let result1 = await pool.request()
-        .input('uid', mssql.Int, user_id)
-        .query('SELECT * from Users WHERE userID = @uid')
-        return (result1);
-    }
 
-
-    function add_user(uid, firstname, lastname, position)
+    function add_user(firstname, lastname, position)
     {
-        const request = new mssql.Request()
-        request.query("INSERT INTO Users (firstName, lastName, position) VALUES "+"('"+uid+"',"+firstname+"', '"+lastname+"', '"+position+"')")
-        .then(result => {
-            console.log(result.rowsAffected)
+        // request.query(`INSERT INTO Users (userID, firstName, lastName, position)
+        //                 VALUES (uid, firstname, lastname, pos)`)
+        const request =  new mssql.Request()
+            request.query("INSERT INTO Users (firstName, lastName, position) VALUES "+"('"+firstname+"', '"+lastname+"', '"+position+"')")
+            .then(result => {
+            console.log(result.rowsAffected);
+            console.log("User added");
         })
     }
 
     function add_user_images(user_id, f_image, r_image, l_image){
-        const request = new mssql.Request()
+        const request =  new mssql.Request()
         request.query("INSERT INTO Reference_images (userID, front_side, right_side, left_side) VALUES ('"+user_id+"', '"+f_image+"', '"+r_image+"','"+l_image+"'')")
         .then(result => {
-            console.log(result.rowsAffected)
+            console.log(result.rowsAffected);
         })
     }
-    
-})()
 
-mssql.on('error', err => {
-    // ... error handle
-    console.log("An error occured");
-})
+     function get_user_by_ID(uid){
+        const request = new mssql.Request()
+            request.query("SELECT * FROM Users WHERE userID = '"+uid+"'")
+            .then(result => {
+            console.log(result);
+            // console.log(result.rowsAffected)
+            return (result.recordset);
+        })
+    }
+
+    function get_users(){
+       const request = new mssql.Request()
+           request.query("SELECT * FROM Users")
+           .then(result => {
+           console.log(result.recordset);
+           console.log(result.rowsAffected)
+           return ( result.recordset);
+       })
+   }
+    // add_user("Yamkela", "Zungula", "Intern");
+    // let user = get_user_by_ID(1);
+    let users = get_users();
+    console.log(users);
+})()
